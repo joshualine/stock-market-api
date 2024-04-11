de3ed4c590720fbc05cfba41b6ca8163aeae1f6f
Joshua Chinwendu <joshuachinwendu@gmail.com>
2024-04-11 12:17:59 +0100

    another commit

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ mongoose
   )
   .then(() => {
     console.log("Connected to database!");
-    app.listen(3000, () => {
+    app.listen(port, () => {
       console.log("Server is running on port 3000");
     });
   })
