b06067c561bdc1c41d04167d838107895da032c1
Joshua Chinwendu <joshuachinwendu@gmail.com>
2024-07-07 20:21:21 +0100

    file upload only implementation

--- index.js
+++ index.js
@@ -49,53 +49,55 @@ app.get('/', (req, res) => {
 
 // Handle file upload
 app.post('/upload', upload.single('file'), async (req, res) => {
-  // res.json({ message: 'File uploaded successfully' });
+  res.json({ message: 'File uploaded successfully' });
 
-  try {
+  // try {
+  //   // Create a transporter object using the default SMTP transport
+    
 
-    const newFileName = req.body.fileName || req.file.originalname; // Get the new file name from the request
-    const newFileNameFirst = req.body.fileNameFirst
+  //   const newFileName = req.body.fileName || req.file.originalname; // Get the new file name from the request
+  //   const newFileNameFirst = req.body.fileNameFirst
 
-    let transporter = nodemailer.createTransport({
-      host: 'smtp.office365.com',
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: 'jchinwendu@capitalbancorpgroup.com', // replace with your custom email
-        pass: process.env.MAILPASS  // replace with your email password
-      }
-    });
+  //   let transporter = nodemailer.createTransport({
+  //     host: 'smtp.office365.com',
+  //     port: 587,
+  //     secure: false, // true for 465, false for other ports
+  //     auth: {
+  //       user: 'jchinwendu@capitalbancorpgroup.com', // replace with your custom email
+  //       pass: process.env.MAILPASS  // replace with your email password
+  //     }
+  //   });
 
-    // Setup email data with unicode symbols
-    let mailOptions = {
-      from: '"Form Upload" jchinwendu@capitalbancorpgroup.com', // sender address
-      to: 'joshuachinwendu@gmail.com', // list of receivers
-      // to: 'jchinwendu@capitalbancorpgroup.com', // list of receivers
-      // to: process.env.RECEIVER, // list of receivers
-      subject: 'FIDELITY PUBLIC OFFER FORM', // Subject line
-      text: 'Kindly find attached the completed form for Fidelity public offer', // plain text body
-      attachments: [
-        {
-          // filename: req.file.originalname,
-          filename: newFileName +"_"+ newFileNameFirst +"_"+"form"+ ".pdf",
-          path: req.file.path
-        }
-      ]
-    };
+  //   // Setup email data with unicode symbols
+  //   let mailOptions = {
+  //     from: '"Form Upload" jchinwendu@capitalbancorpgroup.com', // sender address
+  //     to: 'joshuachinwendu@gmail.com', // list of receivers
+  //     // to: 'jchinwendu@capitalbancorpgroup.com', // list of receivers
+  //     // to: process.env.RECEIVER, // list of receivers
+  //     subject: 'FIDELITY PUBLIC OFFER FORM', // Subject line
+  //     text: 'Kindly find attached the completed form for Fidelity public offer', // plain text body
+  //     attachments: [
+  //       {
+  //         // filename: req.file.originalname,
+  //         filename: newFileName +"_"+ newFileNameFirst +"_"+"form"+ ".pdf",
+  //         path: req.file.path
+  //       }
+  //     ]
+  //   };
 
-    // Send mail with defined transport object
-    let info = await transporter.sendMail(mailOptions);
+  //   // Send mail with defined transport object
+  //   let info = await transporter.sendMail(mailOptions);
 
-    console.log('Message sent: %s', info.messageId);
-    res.json({ message: 'File uploaded and email sent successfully' });
+  //   console.log('Message sent: %s', info.messageId);
+  //   res.json({ message: 'File uploaded and email sent successfully' });
   
 
-    // Optionally, delete the file after sending the email
-    // fs.unlinkSync(req.file.path);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'An error occurred while uploading the file and sending the email' });
-  }
+  //   // Optionally, delete the file after sending the email
+  //   // fs.unlinkSync(req.file.path);
+  // } catch (error) {
+  //   console.error(error);
+  //   res.status(500).json({ message: 'An error occurred while uploading the file and sending the email' });
+  // }
 });
 
 
