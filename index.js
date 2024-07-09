709d2aa5f00b1df5b30550b3363f686ed1fed101
Joshua Chinwendu <43685789+joshualine@users.noreply.github.com>
2024-07-09 16:32:49 +0100

    Update index.js firebase

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const nodemailer = require('nodemailer');
 const path = require('path');
 const fs = require('fs');
 
+const admin = require('firebase-admin');
+const serviceAccount = require('./configs/firebase_config.js');
+// const serviceAccount = require('./configs/firebase_config.json');
+
 
 const stockOverviewRoute = require("./routes/stockOverview.route");
 const userRoute = require("./routes/user.route");
@@ -23,95 +27,103 @@ const port = process.env.PORT || 3000;
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({ credentials: true, origin: '*', optionsSuccessStatus: 200 }));
 
-// Configure storage for multer
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: 'public-offer-project.appspot.com'
+});
+
+const bucket = admin.storage().bucket();
+const upload = multer({ storage: multer.memoryStorage() });
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
   }
-});
 
-const upload = multer({ storage: storage });
+  const blob = bucket.file(req.file.originalname);
+  const blobStream = blob.createWriteStream({
+    metadata: {
+      contentType: req.file.mimetype
+    }
+  });
 
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
-
-// Serve the HTML file
-app.get('/', (req, res) => {
-  // res.sendFile(path.join(__dirname, 'index.html'));
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-// Handle file upload
-app.post('/upload', upload.single('file'), async (req, res) => {
-  res.json({ message: 'File uploaded successfully' });
-
-  // try {
-  //   // Create a transporter object using the default SMTP transport
-    
-
-  //   const newFileName = req.body.fileName || req.file.originalname; // Get the new file name from the request
-  //   const newFileNameFirst = req.body.fileNameFirst
-
-  //   let transporter = nodemailer.createTransport({
-  //     host: 'smtp.office365.com',
-  //     port: 587,
-  //     secure: false, // true for 465, false for other ports
-  //     auth: {
-  //       user: 'jchinwendu@capitalbancorpgroup.com', // replace with your custom email
-  //       pass: process.env.MAILPASS  // replace with your email password
-  //     }
-  //   });
-
-  //   // Setup email data with unicode symbols
-  //   let mailOptions = {
-  //     from: '"Form Upload" jchinwendu@capitalbancorpgroup.com', // sender address
-  //     to: 'joshuachinwendu@gmail.com', // list of receivers
-  //     // to: 'jchinwendu@capitalbancorpgroup.com', // list of receivers
-  //     // to: process.env.RECEIVER, // list of receivers
-  //     subject: 'FIDELITY PUBLIC OFFER FORM', // Subject line
-  //     text: 'Kindly find attached the completed form for Fidelity public offer', // plain text body
-  //     attachments: [
-  //       {
-  //         // filename: req.file.originalname,
-  //         filename: newFileName +"_"+ newFileNameFirst +"_"+"form"+ ".pdf",
-  //         path: req.file.path
-  //       }
-  //     ]
-  //   };
-
-  //   // Send mail with defined transport object
-  //   let info = await transporter.sendMail(mailOptions);
-
-  //   console.log('Message sent: %s', info.messageId);
-  //   res.json({ message: 'File uploaded and email sent successfully' });
+  blobStream.on('error', (err) => res.status(500).send(err));
   
+  blobStream.on('finish', () => {
+    blob.makePublic().then(() => {
+      const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+      res.status(200).send(publicUrl);
+    });
+  });
 
-  //   // Optionally, delete the file after sending the email
-  //   // fs.unlinkSync(req.file.path);
-  // } catch (error) {
-  //   console.error(error);
-  //   res.status(500).json({ message: 'An error occurred while uploading the file and sending the email' });
-  // }
+  blobStream.end(req.file.buffer);
 });
 
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-// app.get('/', (req, res) => {
-//   res.send("Hello from the API")
-// })
+
+// ----------------------------Begining---------------------------------------------------------
+// Set storage engine
+// const storage = multer.diskStorage({
+//   destination: '/home/securitiesadmin/public_html/api/uploads',
+//   filename: function(req, file, cb){
+//     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+//   }
+// });
+
+// // Initialize upload
+// const upload = multer({
+//   storage: storage,
+//   limits: {fileSize: 10000000}, // 10MB limit
+//   fileFilter: function(req, file, cb){
+//     checkFileType(file, cb);
+//   }
+// }).single('myFile');
+
+// // Check file type
+// function checkFileType(file, cb){
+//   const filetypes = /jpeg|jpg|png|gif|pdf/;
+//   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+//   const mimetype = filetypes.test(file.mimetype);
+
+//   if(mimetype && extname){
+//     return cb(null, true);
+//   } else {
+//     cb('Error: Images and PDFs Only!');
+//   }
+// }
+
+// // Route to handle file upload
+// app.post('/upload', (req, res) => {
+//   upload(req, res, (err) => {
+//     if(err){
+//       res.status(400).send(err);
+//     } else {
+//       if(req.file == undefined){
+//         res.status(400).send('No file selected');
+//       } else {
+//         res.send({
+//           message: 'File uploaded successfully',
+//           file: `uploads/${req.file.filename}`
+//         });
+//       }
+//     }
+//   });
+// });
+
+// -------------------------------Ending--------------------------------------------------------
+
+
 
 // routes
 app.use("/api/stockMarket", stockOverviewRoute);
 app.use("/api/users", userRoute);
 app.use("/api/dailyPricelists", dailyPricelistRoute);
 app.use("/api/dailyMarketSummary", dailyMarketSumRoute);
-
-// app.use(express.static(__dirname + '/public'));
 
 
 app.listen(
