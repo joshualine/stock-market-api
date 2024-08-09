2b2522508ecde9c7800ad1aea06bfcbb7d08d1c7
Joshua Chinwendu <joshuachinwendu@gmail.com>
2024-08-09 11:03:46 +0100

    trying to fix the security problem

--- configs/firebase_config.js
+++ configs/firebase_config.js
@@ -3,17 +3,6 @@ dotenv.config();
 
 
 firebaseConfig = {
-  "type": process.env.TYPE,
-  "project_id": process.env.PROJECT_ID,
-  "private_key_id": process.env.PRIVATE_KEY_ID,
-  "private_key": process.env.PRIVATE_KEY,
-  "client_id": process.env.CLIENT_ID,
-  "auth_uri": process.env.AUTH_URI,
-  "token_uri": process.env.TOKEN_URI,
-  "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER,
-  "client_x509_cert_url": process.env.CLIENT_CERT,
-  "universe_domain": process.env.UNIVERSE_DOMAIN,
-  "client_email": process.env.EMAIL_CLIENT,
 }
 
 module.exports = firebaseConfig;
